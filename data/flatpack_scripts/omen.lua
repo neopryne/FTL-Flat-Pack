ed76f4397f2be12d1afe56f60e8c597458f1db83
neopryne <nope@>
2024-10-18 19:24:39 -0400

    Added laser, cleaned code
    Made things more modular and self contained.
    Laser might work soon.  Burst needs particles.
    
    Manning needs particles.

--- data/flatpack_scripts/omen.lua
+++ data/flatpack_scripts/omen.lua
@@ -1,12 +1,61 @@
 local userdata_table = mods.multiverse.userdata_table
 local vter = mods.multiverse.vter
+local get_room_at_location = mods.vertexutil.get_room_at_location
+--[[next:
+        checking what omen's doing
+        adjusting rotations based on that
+        fighting
+        anything else
+        add the EYE
+        
+        big laser
+        
+        stretch goal: green/white outlines when selected/manning while selected
+        This is a slightly larger green/yellow prism on its own face scene
+        uniqueing this for omen name?
+--]]
 
 local global = Hyperspace.Global.GetInstance()
 local soundControl = global:GetSoundControl()
 
+--from fishing
+local function isPaused()
+    local cmdGui = Hyperspace.Global.GetInstance():GetCApp().gui
+    return cmdGui.event_pause or cmdGui.menu_pause
+end
 
---need to add to userdata so you can have two.
---uniqueing this for omen name?
+local function get_room_at_crewmember(crewmem)
+    local shipManager = global:GetShipManager(crewmem.iShipId)
+    return get_room_at_location(shipManager, crewmem:GetPosition(), false)
+end
+
+local function damage_enemy_helper(activeCrew, amount, currentRoom, bystander)
+      --print(bystander:GetLongName(), "room ", bystander.iRoomId, " ", currentRoom, " ", bystander.currentShipId == activeCrew.currentShipId)
+    if bystander.iRoomId == currentRoom and bystander.iShipId == ENEMY_SHIP and bystander.currentShipId == activeCrew.currentShipId then
+        --print(bystander:GetLongName(), " was in the same room!")
+        bystander:DirectModifyHealth(-amount)
+    end
+end
+
+--TODO pull into library, used by: omen, f22, add friendly fire and other options
+local function damage_enemy_crew_in_same_room(activeCrew, amount)
+    local currentRoom = get_room_at_crewmember(activeCrew)
+        -- Modified from brightlord's modification of Arc's get_ship_crew_room().
+    if (Hyperspace.ships.enemy) then
+      for bystander in vter(Hyperspace.ships.enemy.vCrewList) do
+          damage_enemy_helper(activeCrew, currentRoom, bystander)
+      end
+    end
+    --do the same for friendly ship
+    for bystander in vter(Hyperspace.ships.player.vCrewList) do
+        damage_enemy_helper(activeCrew, currentRoom, bystander)
+    end
+end
+
+local MAX_POWER = 100
+local BASE_BEAM_DAMAGE = 25
+local BASE_BLAST_DAMAGE = 20
+local BEAM_TIME = 76
 
 local X_RENDER_OFFSET = -15
 local Y_RENDER_OFFSET = -15
@@ -18,14 +67,16 @@ local TOP_POINT_Y = TOP_POINT_X * math.tan(math.pi/3) --60 degrees
 local EYE_ANGLE = math.pi/6
 local IRIS_ANGLE = math.pi/3
 local EYE_Z = -1 --just on top of everything
+local BEAM_Z = -76
 local LEFT_EYE_X = (TOP_POINT_X / 3)
 local LEFT_EYE_Y = (TOP_POINT_Y / 3)
 local RIGHT_EYE_X = OUTER_SQUARE_SIZE - LEFT_EYE_X
 local RIGHT_EYE_Y = LEFT_EYE_Y
 local TOP_EYE_X = TOP_POINT_X
-local TOP_EYE_Y = LEFT_EYE_Y + (math.tan(EYE_ANGLE / 2) * (TOP_EYE_X - LEFT_EYE_X))
+local TOP_EYE_OFFSET_Y = (math.tan(EYE_ANGLE) * (TOP_EYE_X - LEFT_EYE_X))
+local TOP_EYE_Y = LEFT_EYE_Y + TOP_EYE_OFFSET_Y
 local BOTTOM_EYE_X = TOP_POINT_X
-local BOTTOM_EYE_Y = TOP_EYE_Y - LEFT_EYE_Y
+local BOTTOM_EYE_Y = LEFT_EYE_Y - TOP_EYE_OFFSET_Y
 
 local INNER_EYE_OFFSET_X = (math.tan(IRIS_ANGLE / 2) * (TOP_EYE_Y - LEFT_EYE_Y))
 local INNER_EYE_RIGHT_X = TOP_EYE_X + INNER_EYE_OFFSET_X
@@ -39,11 +90,12 @@ print("c ", CENTER_POINT.x, CENTER_POINT.y, CENTER_POINT.z)
 
 local ROTATIONS = {x = .01, y = 0, z = .007}
 
-local OMEN_BODY_COLOR = Graphics.GL_Color(.8, .8, .8, 1) --a
-local COLOR2 = Graphics.GL_Color(.8, .8, .8, 1) --a
+local OMEN_BODY_COLOR = Graphics.GL_Color(.8, .8, .8, 1)
+local WHITE = Graphics.GL_Color(.9, .9, .9, 1) 
 local GREY50 = Graphics.GL_Color(0, 0, 0, .5)
-local BLACK = Graphics.GL_Color(0, 0, 0, 1) --a
+local BLACK = Graphics.GL_Color(0, 0, 0, 1) 
 
+--todo may need to deep copy/hardcode
 local INITIAL_PRISM = {
     -- Front triangle vertices (x, y, z)
     {x = 0, y = 0, z = 0},
@@ -62,11 +114,14 @@ local INITIAL_PRISM = {
     {x = BOTTOM_EYE_X, y = BOTTOM_EYE_Y, z = EYE_Z},
     --Inner Eye
     {x = INNER_EYE_LEFT_X, y = INNER_EYE_LEFT_Y, z = EYE_Z},
-    {x = INNER_EYE_RIGHT_X, y = INNER_EYE_RIGHT_Y, z = EYE_Z}
+    {x = INNER_EYE_RIGHT_X, y = INNER_EYE_RIGHT_Y, z = EYE_Z},
+    
+    --Beam points, not usually rendered.  Comes out of the Inner Eye.
+    {x = INNER_EYE_LEFT_X, y = INNER_EYE_LEFT_Y, z = BEAM_Z},
+    {x = TOP_EYE_X, y = TOP_EYE_Y, z = BEAM_Z},
+    {x = INNER_EYE_RIGHT_X, y = INNER_EYE_RIGHT_Y, z = BEAM_Z},
+    {x = BOTTOM_EYE_X, y = BOTTOM_EYE_Y, z = BEAM_Z}
 }
-
-local prism = INITIAL_PRISM
-
 
 -- Faces of the triangular prism (each face is defined by a set of vertex indices)
 local faces = {
@@ -77,6 +132,14 @@ local faces = {
     {3, 1, 4, 6, fill_color = OMEN_BODY_COLOR, filled = true},      -- Third side (quad)
     {7, 8, 9, 10, outline_color = BLACK, outline = true, line_width=2},      -- Eye outline (quad)  need new render layer value for this on top
     {11, 8, 12, 10, fill_color = BLACK, filled = true}      -- Eye outline (quad)  
+}
+
+local beam_faces = {
+    {13, 14, 15, 16, fill_color = WHITE, filled = true}, -- endcap
+    {13, 14, 8, 11, fill_color = WHITE, filled = true}, -- faces
+    {15, 14, 8, 12, fill_color = WHITE, filled = true}, -- faces
+    {15, 16, 10, 12, fill_color = WHITE, filled = true}, -- faces
+    {13, 16, 10, 11, fill_color = WHITE, filled = true}, -- faces
 }
 
 -- Helper function to rotate a point around a fixed point
@@ -114,7 +177,7 @@ function rotatePointAroundFixed(p, cx, cy, cz, angleX, angleY, angleZ)
     return {x = x + cx, y = y + cy, z = z + cz}
 end
 
---cs are values of center point of the prism.  Actually you can just calculate this.
+--cs are values of center point of the prism.
 function rotateAround(object, cx, cy, cz, angleX, angleY, angleZ)
     local rotatedObject = {}
     for i, vertex in ipairs(object) do
@@ -126,19 +189,19 @@ end
 
 
     -- Sort faces by their average z-depth (for simple face culling)
-function sortFacesByDepth()
-    table.sort(faces, function(f1, f2)
+function sortFacesByDepth(multifacedObject, active_faces)
+    table.sort(active_faces, function(f1, f2)
         -- Compute average z for face f1
         local z1 = 0
         for _, index in ipairs(f1) do
-            z1 = z1 + prism[index].z
+            z1 = z1 + multifacedObject[index].z
         end
         z1 = z1 / #f1
 
         -- Compute average z for face f2
         local z2 = 0
         for _, index in ipairs(f2) do
-            z2 = z2 + prism[index].z
+            z2 = z2 + multifacedObject[index].z
         end
         z2 = z2 / #f2
 
@@ -160,14 +223,14 @@ local function relativeVertex(vertex, position)
     return Hyperspace.Point(relativeX(vertex.x, position), relativeY(vertex.y, position))
 end
 
-local function relativeVertexByIndex(vertexIndex, position)
+local function relativeVertexByIndex(prism, vertexIndex, position)
     return relativeVertex(prism[vertexIndex], position)
 end
 
-local function drawRelativeLine(vertex1, vertex2, position, line_width, color)
+local function drawRelativeLine(prism, vertex1, vertex2, position, line_width, color)
     --Graphics.CSurface.GL_DrawLine(prism[vertex1].x + position.x,  prism[vertex1].y + position.y, prism[vertex2].x + position.x,  prism[vertex2].y + position.y, 2, BLACK)
     Graphics.CSurface.GL_DrawLine(relativeX(prism[vertex1].x, position),  relativeY(prism[vertex1].y, position), 
-            relativeX(prism[vertex2].x, position),  relativeY(prism[vertex2].y, position), 1, GREY50)
+            relativeX(prism[vertex2].x, position),  relativeY(prism[vertex2].y, position), line_width, GREY50)
 end
 
 --slightly different bc idk how to do overloading
@@ -178,13 +241,13 @@ local function drawRelativeLine2(vertex1, point1, position)
 end
 
 
-local function glDrawTriangle_Wrapper(vertex1, vertex2, vertex3, position, color)
-    point1 = relativeVertexByIndex(vertex1, position)
-    point2 = relativeVertexByIndex(vertex2, position)
-    point3 = relativeVertexByIndex(vertex3, position)
+local function glDrawTriangle_Wrapper(prism, vertex1, vertex2, vertex3, position, color)
+    point1 = relativeVertexByIndex(prism, vertex1, position)
+    point2 = relativeVertexByIndex(prism, vertex2, position)
+    point3 = relativeVertexByIndex(prism, vertex3, position)
     
     --print("rendering triangle", point1.x, ", ", point1.y, " -- ", point2.x, ", ", point2.y, " -- ", point3.x, ", ", point3.y)
-    Graphics.CSurface.GL_DrawTriangle(relativeVertexByIndex(vertex1, position), relativeVertexByIndex(vertex2, position), relativeVertexByIndex(vertex3, position), color)
+    Graphics.CSurface.GL_DrawTriangle(point1, point2, point3, color)
     --draw black lines
     --drawRelativeLine(vertex1, vertex2, position, color)
     --drawRelativeLine(vertex2, vertex3, position, color)
@@ -195,83 +258,137 @@ end
 
 --requires that the face points are in order and a convex polygon
 --only works for three or four points
-local function drawFace(face, position)
+--maybe call this prism the mesh in lib
+local function drawFace(prism, face, position)
     for i = 3, #face do
         --print("drawing triangle ", i)
         if (face.filled) then
-            glDrawTriangle_Wrapper(face[1], face[i-1], face[i], position, face.fill_color)
+            glDrawTriangle_Wrapper(prism, face[1], face[i-1], face[i], position, face.fill_color)
         end
         if (face.outline) then
-            drawRelativeLine(face[i-1], face[i], position, face.line_width, face.outline_color)
+            drawRelativeLine(prism, face[i-1], face[i], position, face.line_width, face.outline_color)
         end
     end
     if (face.outline) then
-        drawRelativeLine(face[1], face[#face], position, face.line_width, face.outline_color)
-        drawRelativeLine(face[1], face[2], position, face.line_width, face.outline_color)
+        drawRelativeLine(prism, face[1], face[#face], position, face.line_width, face.outline_color)
+        drawRelativeLine(prism, face[1], face[2], position, face.line_width, face.outline_color)
     end
 end
 
-
-
-function drawOmen(position)
-    -- Sort the faces by depth
-    --print("draw omen")
-    sortFacesByDepth()
-
+function drawObject(position, object_points, object_faces)
+    sortFacesByDepth(object_points, object_faces)
+    --all rendering must be done between pop/push actions it seems?  Actually I have no idea what these do.
     -- Draw faces (filled polygons)
     Graphics.CSurface.GL_PushMatrix()
-    for i, face in ipairs(faces) do
-        drawFace(face, position)
+    for i, face in ipairs(object_faces) do
+        drawFace(object_points, face, position)
     end
     Graphics.CSurface.GL_PopMatrix()
-
-    -- Optionally draw edges after filling faces (for clearer visual edges)
-    --this needs to be done at the same time as the face or it will render wrong.
-    --setColor(0, 0, 0) -- Black for edges
-    --[[for i, face in ipairs(faces) do
-        local vertices = {}
-        for _, index in ipairs(face) do
-            local v = project3Dto2D(prism[index])
-            table.insert(vertices, v.x)
-            table.insert(vertices, v.y)
-        end
-        love.graphics.polygon("line", vertices) -- Draw the outline
-    end--]]
 end
 
+function drawBeam(position, prism)
+    drawObject(position, prism, beam_faces)
+end
 
---[[next:
-        checking what omen's doing
-        adjusting rotations based on that
-        fighting
-        anything else
-        add the EYE
-        
-        big laser?
---]]
+function drawOmen(position, prism)
+    drawObject(position, prism, faces)
+end
+
+function randomRotation()
+    return {x = math.random() * .01, y = math.random() * .01, z = math.random() * .01}
+end
 
 --only functions on player ship
 script.on_render_event(Defines.RenderEvents.SHIP_MANAGER, function() end, function(ship)
-    --local crewTable = userdata_table(crewmem, "mods.flatpack.fatespinner")
     local shipManager = global:GetShipManager(ship.iShipId)
     for crewmem in vter(shipManager.vCrewList) do
         if (crewmem:GetSpecies() == "fff_omen") then
-            --todo only run if unpaused.  Breaking pause is for later.  extemporality upgrade in the lab disables this check.
-            
-        --print("found omen")
-        --all but the timer do what I want.  skilling is training skills, and tells you what room they are manning.
-        --print("Shoot timer: ", crewmem.crewAnim.shootTimer, " shared spot: ", crewmem.bSharedSpot, " fighting: ", crewmem.bFighting, " maning: ", crewmem.bActiveManning, "skiling: ", crewmem.usingSkill)
-        --explodes in melee
-        --uses laser/beam at range
-        --slow deceleration when not fighting
-        --powered up = rotation speed
-        
-      --render oh god
-      --so much needs to go in the table
-      --actually just the rotation and the rotation speed?
-            pos = crewmem:GetPosition()
-            drawOmen(pos)
-            prism = rotateAround(prism, CENTER_POINT.x, CENTER_POINT.y, CENTER_POINT.z, ROTATIONS.x, ROTATIONS.y, ROTATIONS.z)
+            if (not isPaused()) then --this doesn't seem to do anything
+                local crewTable = userdata_table(crewmem, "mods.flatpack.fatespinner")
+                pos = crewmem:GetPosition()
+
+                
+                --VARIABLE DEFINITIONS
+                local prism_model = crewTable.prism_model
+                if (not prism_model) then
+                    prism_model = INITIAL_PRISM
+                end
+                
+                local rotations = crewTable.rotations
+                if not (rotations) then
+                    rotations = randomRotation()
+                end
+                
+                --CRUNCHY STUFF, maybe goes in its own method
+                local omen_power
+                if not (crewTable.omen_power) then --if undefined
+                    omen_power = 1
+                else
+                    omen_power = crewTable.omen_power
+                end
+                
+                local beam_render_time
+                if not (crewTable.beam_render_time) then --if undefined
+                    beam_render_time = 0
+                else
+                    beam_render_time = crewTable.beam_render_time
+                end
+                
+                --when entering combat, pick new rotation direction and scale it up by POWER until the total power is MAX_POWER
+                local is_combat = crewmem.bFighting
+                if (is_combat) then
+                    if (not (is_combat == crewTable.was_combat)) then
+                        --rerandomize direction on entering combat
+                        rotations = randomRotation()
+                        soundControl:PlaySoundMix("fff_omen_spinup", 2, false)
+                    end
+                    --99 MOB CHORUS
+                    omen_power = omen_power + .1, MAX_POWER
+                    if (omen_power >= MAX_POWER) then --go off!
+                        if (crewmem.bSharedSpot) then
+                            print("OMEN BLAST TRIGGERED")
+                            soundControl:PlaySoundMix("fff_omen_blast", 3, false)
+                            --melee
+                            damage_enemy_crew_in_same_room(crewmem, BASE_BLAST_DAMAGE)
+                            --brightness particle stuff
+                        else
+                            --ranged
+                            print("OMEN BEAM TRIGGERED")
+                            soundControl:PlaySoundMix("fff_omen_shoot", 3, false)
+                            crewTable.beam_render_time = BEAM_TIME
+                            --draw a random triangle/hyperbola and damage in it.
+                        end
+                        omen_power = 50
+                    end
+                else
+                    omen_power = math.max(omen_power - .05, 1)
+                end
+                
+                
+                 
+                 
+                 
+                 
+                if (crewmem.bActiveManning) then
+                    --render some particles based on skiling value
+                end
+                
+                
+                
+                crewTable.was_combat = is_combat
+                crewTable.omen_power = omen_power
+                crewTable.rotations = rotations
+                --todo only run if unpaused.  Breaking pause is for later.  extemporality upgrade in the lab disables this check.
+                if (beam_render_time > 0) then
+                    drawBeam(pos, prism_model)
+                    beam_render_time = beam_render_time - 1
+                    crewTable.beam_render_time = beam_render_time
+                end
+                
+                drawOmen(pos, prism_model)
+                prism_model = rotateAround(prism_model, CENTER_POINT.x, CENTER_POINT.y, CENTER_POINT.z, rotations.x * omen_power, rotations.y * omen_power, rotations.z * omen_power)
+                crewTable.prism_model = prism_model
+            end
         end
     end
 end)
